86a9355ed11d4674ec060a8dc6b7cf84c19236af
grzspychala <grzspychala@gmail.com>
2015-10-14 22:03:55 +0200

    gui init

--- spyker/main.py
+++ spyker/main.py
@@ -1,5 +1,12 @@
-from spyker.tkgui import *
+from spyker.gui.tkgui import *
+import tkinter
 
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+    root = Tk()
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+
+    MainFrame(root, padding=(3, 3, 12, 12))
+    menubar = MenuBar(root)
+    root.config(menu=menubar)
+    root.mainloop()
