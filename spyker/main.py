94db122919e33eda91f7d6d7a2cfbb272e608fc3
pnikrat <sniffoza@gmail.com>
2016-01-17 15:36:53 +0100

    added icons

--- spyker/main.py
+++ spyker/main.py
@@ -5,7 +5,7 @@ from dircache import listdir
 from os.path import isfile
 from os.path import join
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 import spyker.model.charts as plots
 from spyker.gui.mainwindow import MainWindow
@@ -25,12 +25,21 @@ if __name__ == '__main__':
     chart_list_model = ChartListModel(chart_dict)
 
     file_list_model = FileListModel()
+
     if os.path.exists(RECS_DIR):
         for f in listdir(RECS_DIR):
             if isfile(join(RECS_DIR, f)):
                 file_list_model.insertRows(f)
 
     app = QtGui.QApplication(sys.argv)
+    app_icon = QtGui.QIcon()
+    app_icon.addFile('icons/16.png', QtCore.QSize(16, 16))
+    app_icon.addFile('icons/24.png', QtCore.QSize(24, 24))
+    app_icon.addFile('icons/32.png', QtCore.QSize(32, 32))
+    app_icon.addFile('icons/64.png', QtCore.QSize(64, 64))
+    app_icon.addFile('icons/128.png', QtCore.QSize(128, 128))
+    app_icon.addFile('icons/256.png', QtCore.QSize(256, 256))
+    app.setWindowIcon(app_icon)
     main_window = MainWindow(file_list_model, chart_list_model)
     main_window.show()
     sys.exit(app.exec_())
